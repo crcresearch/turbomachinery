0b63a10d3254c022c428fffca5c7dfa9f001db38
root <root@turbo.virtual.crc.nd.edu>
2019-04-23 15:29:52 -0400

    from prod

--- pr/urls.py
+++ pr/urls.py
@@ -31,7 +31,7 @@ from time_management.rates import rates_home, save_rate, save_start_date, save_e
     add_rates, add_single_category
 from time_management.auth import login_page, logout_request
 
-# import cas.views
+import cas.views
 from django.conf import settings
 from time_management.team_management import team_management, get_teams, get_team, save_manager, remove_team_member, \
     add_team_member, remove_team, create_team
@@ -113,16 +113,14 @@ urlpatterns = [
     url(r'^weekly_report/$', weekly_report_form_url, name='weekly_report'),
 
     # Used if on production for CAS authentication
-    # url(r'^login/$', cas.views.login, name='login'),
-    # url(r'^logout/$', cas.views.logout, name='logout'),
+    url(r'^login/$', cas.views.login, name='login'),
+    url(r'^logout/$', cas.views.logout, name='logout'),
 
     # OR you can use these login pages:
     #url(r'^accounts/login/$', cas.views.login, name='account_login'),
     #url(r'^accounts/logout/$', cas.views.logout, name='account_logout'),
     
-    url(r'^login/$', login_page, name='login'),
-    url(r'^logout/$', logout_request, name='logout'),
-
-
+    #url(r'^login/$', login_page, name='login'),
+    #url(r'^logout/$', logout_request, name='logout'),
 
 ]
