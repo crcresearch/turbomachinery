0145d3559d7f800b63f286b7fca315a0bb85bf30
srucker01 <srucker94@gmail.com>
2025-03-24 15:20:56 -0400

    monthly report

--- time_management/templatetags/report_filters.py
+++ time_management/templatetags/report_filters.py
@@ -2,7 +2,9 @@ from django import template
 
 register = template.Library()
 
-@register.filter
+@register.filter(name='get_item')
 def get_item(dictionary, key):
     """Get item from dictionary by key"""
+    if not dictionary:
+        return None
     return dictionary.get(key) 
